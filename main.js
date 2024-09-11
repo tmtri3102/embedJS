a94f8d11157d6cc5af43e7c2846694cee60d7d9b
tomeytran <tmtri310251@gmail.com>
2024-09-11 15:06:18 +0700

    commit

---
+++ main.js
@@ -0,0 +1,7 @@
+function submitBtn() {
+	const value = prompt("Nhập tên");
+	const confirmation = confirm("B chac chua");
+	document.write(value);
+	console.log(value);
+	console.log(confirmation);
+}
